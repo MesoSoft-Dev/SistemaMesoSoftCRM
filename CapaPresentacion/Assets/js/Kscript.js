0b516dc8fe10fc3fe6269163cd8fd43bb5b4b451
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-11-02 18:06:23 -0600

    Problemas con el javascript solucionados.

--- CapaPresentacion/Assets/js/Kscript.js
+++ CapaPresentacion/Assets/js/Kscript.js
@@ -3,106 +3,111 @@ const all_status = document.querySelectorAll(".status");
 let draggableTodo = null;
 
 todos.forEach((todo) => {
-  todo.addEventListener("dragstart", dragStart);
-  todo.addEventListener("dragend", dragEnd);
+    todo.addEventListener("dragstart", dragStart);
+    todo.addEventListener("dragend", dragEnd);
 });
 
 function dragStart() {
-  draggableTodo = this;
-  setTimeout(() => {
-    this.style.display = "none";
-  }, 0);
-  console.log("dragStart");
+    draggableTodo = this;
+    setTimeout(() => {
+        this.style.display = "none";
+    }, 0);
+    console.log("dragStart");
 }
 
 function dragEnd() {
-  draggableTodo = null;
-  setTimeout(() => {
-    this.style.display = "block";
-  }, 0);
-  console.log("dragEnd");
+    draggableTodo = null;
+    setTimeout(() => {
+        this.style.display = "block";
+    }, 0);
+    console.log("dragEnd");
 }
 
 all_status.forEach((status) => {
-  status.addEventListener("dragover", dragOver);
-  status.addEventListener("dragenter", dragEnter);
-  status.addEventListener("dragleave", dragLeave);
-  status.addEventListener("drop", dragDrop);
+    status.addEventListener("dragover", dragOver);
+    status.addEventListener("dragenter", dragEnter);
+    status.addEventListener("dragleave", dragLeave);
+    status.addEventListener("drop", dragDrop);
 });
 
 function dragOver(e) {
-  e.preventDefault();
-  //   console.log("dragOver");
+    e.preventDefault();
 }
 
 function dragEnter() {
-  this.style.border = "1px dashed #ccc";
-  console.log("dragEnter");
+    this.style.border = "1px dashed #ccc";
+    console.log("dragEnter");
 }
 
 function dragLeave() {
-  this.style.border = "none";
-  console.log("dragLeave");
+    this.style.border = "none";
+    console.log("dragLeave");
 }
 
 function dragDrop() {
-  this.style.border = "none";
-  this.appendChild(draggableTodo);
-  console.log("dropped");
+    this.style.border = "none";
+    this.appendChild(draggableTodo);
+    console.log("dropped");
 }
 
-/* modal */
+/* Modal */
 const btns = document.querySelectorAll("[data-target-modal]");
 const close_modals = document.querySelectorAll(".close-modal");
 const overlay = document.getElementById("overlay");
 
 btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(btn.dataset.targetModal).classList.add("active");
-    overlay.classList.add("active");
-  });
+    btn.addEventListener("click", () => {
+        document.querySelector(btn.dataset.targetModal).classList.add("active");
+        overlay.classList.add("active");
+    });
 });
 
 close_modals.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  });
+    btn.addEventListener("click", () => {
+        const modal = btn.closest(".modal");
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    });
 });
 
 window.onclick = (event) => {
-  if (event.target == overlay) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => modal.classList.remove("active"));
-    overlay.classList.remove("active");
-  }
+    if (event.target == overlay) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => modal.classList.remove("active"));
+        overlay.classList.remove("active");
+    }
 };
 
-/* create todo  */
+/* Crear nueva tarea */
 const todo_submit = document.getElementById("todo_submit");
 
 todo_submit.addEventListener("click", createTodo);
 
 function createTodo() {
-  const todo_div = document.createElement("div");
-  const input_val = document.getElementById("todo_input").value;
-  const txt = document.createTextNode(input_val);
+    const input_val = document.getElementById("todo_input").value;
 
-  todo_div.appendChild(txt);
-  todo_div.classList.add("todo");
-  todo_div.setAttribute("draggable", "true");
+    if (input_val.trim() !== "") { 
+       
+        const todo_div = document.createElement("div");
+        const txt = document.createTextNode(input_val);
 
-  no_status.appendChild(todo_div);
+        todo_div.appendChild(txt);
+        todo_div.classList.add("todo");
+        todo_div.setAttribute("draggable", "true");
 
-  todo_div.addEventListener("dragstart", dragStart);
-  todo_div.addEventListener("dragend", dragEnd);
+        
+        document.getElementById("no_status").appendChild(todo_div);
 
-  document.getElementById("todo_input").value = "";
-  todo_form.classList.remove("active");
-  overlay.classList.remove("active");
+     
+        todo_div.addEventListener("dragstart", dragStart);
+        todo_div.addEventListener("dragend", dragEnd);
 
-  const modalElement = document.getElementById('todo_form');
-  const modalInstance = bootstrap.Modal.getInstance(modalElement);
-  modalInstance.hide();
+      
+        document.getElementById("todo_input").value = "";
+
+        
+        const modalElement = document.getElementById('todo_form');
+        const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement); 
+        modalInstance.hide(); 
+    }
 }
--- CapaPresentacion/kanban.aspx
+++ CapaPresentacion/kanban.aspx
@@ -1,53 +1,57 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="kanban.aspx.cs" Inherits="CapaPresentacion.kanban" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-        <link rel="stylesheet" type="text/css" href="Assets/css/Kstyle.css" />
-
-
+    <link rel="stylesheet" type="text/css" href="Assets/css/Kstyle.css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    
-    <div class="modal fade" id="todo_form" tabindex="-1" aria-labelledby="todoModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="todoModalLabel">Agregar tarea</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <input type="text" id="todo_input" class="form-control mb-2" />
-            <button class="btn btn-success btn-block" id="todo_submit">Agregar tarea</button>
-          </div>
+    <form id="form1" runat="server">
+        
+     
+        <div class="mx-3">
+            <button id="btn+oportunidad" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#todo_form" type="button">
+                + Oportunidad
+            </button>
         </div>
-      </div>
-    </div>
-    <div class="container mt-5 ">
-      <div class="row mx-auto"> 
-        <div class="col-md status border shadow mb-3 me-3" id="no_status">
-          <h2 class="">No iniciado</h2>
-          <button id="add_btn" class="btn w-100 btn-agregar mt-auto rounded-0  " data-bs-toggle="modal" data-bs-target="#todo_form">
-            + Agregar
-          </button>
+        
+      
+        <div class="modal fade" id="todo_form" tabindex="-1" aria-labelledby="todoModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="todoModalLabel">Agregar tarea</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <input type="text" id="todo_input" class="form-control mb-2" />
+                        
+                     
+                        <button type="button" class="btn btn-success btn-block" id="todo_submit">Agregar tarea</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md border shadow status  mb-3 me-3">
-          <h2>En progreso</h2>
-        </div>
-        <div class="col-md border shadow status mb-3 me-3">
-          <h2>Completado</h2>
-        </div>
-        <div class="col-md border shadow status mb-3 me-3">
-          <h2>Cancelado</h2>
-        </div>
-        <div class="col-md border shadow status mb-3 me-3">
-          <h2>En espera</h2>
-        </div>
-      </div>
-    </div>
 
-    <div id="overlay" class="d-none"></div>
+        <div class="container mt-4">
+            <div class="row mx-auto"> 
+                <div class="col-md status border shadow mb-3 me-3" id="no_status">
+                    <h2>No iniciado</h2>
+                </div>
+                <div class="col-md border shadow status mb-3 me-3">
+                    <h2>En progreso</h2>
+                </div>
+                <div class="col-md border shadow status mb-3 me-3">
+                    <h2>Completado</h2>
+                </div>
+                <div class="col-md border shadow status mb-3 me-3">
+                    <h2>Cancelado</h2>
+                </div>
+                <div class="col-md border shadow status mb-3 me-3">
+                    <h2>En espera</h2>
+                </div>
+            </div>
+        </div>
 
-<script src="Assets/js/Kscript.js"></script>
-   
-  
- 
+        <div id="overlay" class="d-none"></div>
+    </form>
 
+    <script src="Assets/js/Kscript.js"></script>
 </asp:Content>
