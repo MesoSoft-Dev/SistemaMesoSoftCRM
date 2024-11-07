0e8c0d46203ad8991317da9e346d0ac3b5a46855
Enrique00007-uam <emsolis@uamv.edu.ni>
2024-11-06 18:41:40 -0600

    Funcion de resaltar la pagina activa agregada

--- CapaPresentacion/Assets/js/script.js
+++ CapaPresentacion/Assets/js/script.js
@@ -15,3 +15,18 @@ function showLogin() {
     document.getElementById('changePasswordSection').classList.add('hidden');
     document.getElementById('forgotPasswordSection').classList.add('hidden');
 }
+
+//funcion de clase active
+document.addEventListener("DOMContentLoaded", function () {
+    var currentPage = window.location.pathname.split("/").pop().replace('.aspx', '');
+
+    var navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(function (link) {
+        link.classList.remove('active');
+    });
+
+    var activeLink = document.getElementById(currentPage);
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+});
